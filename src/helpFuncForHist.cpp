a4ac50c0357de1765e7f4c87cda45995a32d4de8
Artem <klufone@mail.ru>
2017-03-24 00:49:35 +0300

    rename file and delete the trash

--- src/helpFuncForHist.cpp
+++ src/helpFuncForHist.cpp
@@ -7,7 +7,7 @@
 //
 
 #include <stdio.h>
-
+//x
 void initHistArr(uint *_histArr){
     for (int i =0; i<256; i++) {
         *(_histArr + i) = 0;
@@ -23,24 +23,6 @@ void fillArrayWithValuesFromGI(uint *_histArr ,Mat _grayScaleImageMatrix){
     }
 }
 //////////////////////////////////////////////////////////////////////////////////////////
-void printHistArray(uint *_histArr){
-    for (int i =0; i<256; i++) {
-        std::cout<<*(_histArr+i)<<" - "<<i<<std::endl;
-    }
-}
-//////////////////////////////////////////////////////////////////////////////////////////
-uint findMinValueInHistArrayAndFixIndex(uint *_histArr,uint *_ptrToTheFlagVariable) {
-    uint tempMin = *(_histArr);
-    for (int i =0; i<256; i++) {
-        if (*(_histArr+i) <= tempMin ) {
-            tempMin = *(_histArr+i);
-            *(_ptrToTheFlagVariable) = i;
-        }
-        
-    }
-    return tempMin;
-}
-//////////////////////////////////////////////////////////////////////////////////////////
 uint findMaxValueInHistArrayAndFixIndex(uint* _histArr,uint *_ptrToTheFlagVariable) {
     uint tempMax = *(_histArr);
     for (int i =0; i<256; i++) {
@@ -54,41 +36,3 @@ uint findMaxValueInHistArrayAndFixIndex(uint* _histArr,uint *_ptrToTheFlagVariab
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
-int checkCorrect(int _value,int min,int max){
-    if ( (_value <= max) && (_value >= min) ) {
-        return true;
-    }
-    else return false;
-}
-//////////////////////////////////////////////////////////////////////////////////////////
-void ConvertImageToBinaryWithBarrier(Mat_<uchar> _imagePtr,int _barrier ) {
-    for (int i  = 0 ; i < _imagePtr.rows; i++) {
-        for (int j = 0 ; j < _imagePtr.cols; j++) {
-            if (_imagePtr(i,j) > _barrier) {
-                _imagePtr(i,j)= 255;
-            }else {
-                _imagePtr(i,j)= 0;
-            }
-        }
-    }
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////
-void compareTwoMat(const Mat firstMat, const Mat secondMat) {
-    Mat differenceImage = firstMat.clone();
-    for (int i = 0; i < differenceImage.rows; i++) {
-        for (int j = 0; j < differenceImage.cols; j++) {
-            if (firstMat.at<Vec3b>(i, j) != secondMat.at<cv::Vec3b>(i, j)) {
-                differenceImage.at<cv::Vec3b>(i, j) = Vec3b(255, 0, 0);
-            } else {
-                differenceImage.at<cv::Vec3b>(i, j) = firstMat.at<cv::Vec3b>(i, j);
-            }
-        }
-    }
-    
-    imshow("Image difference", differenceImage);
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////
-
-
